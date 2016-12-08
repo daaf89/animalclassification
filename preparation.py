7f511c01eb7ee41df4fcb6e6cc3d2a15b618508f
David Langerveld <david.langerveld@student.auc.nl>
2016-12-08 16:54:38 +0100

    preparation now also prepares the sift points for the validation set. Running it will also save the histograms in a file, for easy access.

--- preparation.py
+++ preparation.py
@@ -18,6 +18,11 @@ trainimages = [line.strip().split(" ")[0] for
                line in open("trainset-overview.txt", "r")]
 print "There are", len(trainimages), "training images"
 
+"""
+Given an image, the function will calculate the Hes- and HarSiftpoints and
+return them sift1. point1 contains the locations of the sift points in the
+original image.
+"""
 def extract_sift(trainimage):
     sigma       = 1.0
     hespoints   = sift.computeHes(trainimage, sigma, magThreshold=15,
@@ -29,6 +34,7 @@ def extract_sift(trainimage):
                                              nrOrientBins=8, nrSpatBins=4,
                                              nrPixPerBin=4)
     return point1, sift1
+
 
 # Put the SIFT features from all training images in this variable.
 trainpoints = []
@@ -43,6 +49,7 @@ for i in xrange(len(trainimages)):
 # Cluster the SIFT features and put them in a matrix with the name 'clusters'!
 print "Clustering..."
 
+
 def cluster_data(features, k, nr_iter=25):
     centroids = k_means(features, n_clusters=k, max_iter=nr_iter)[0]
     return centroids
@@ -52,10 +59,13 @@ k = 100
 clusters = cluster_data(trainpoints, k)
 print 'done!'
 
+"""Simple function to calculate Euclidean distance"""
 def euclidean_distance(x, y):
     assert(len(x) == len(y))
     return np.sum((x-y)**2)**.5
 
+"""Simple function which will calculate the distances between a single image,
+'a', and the X matrix of images."""
 def distances(a,X,distance_fn=euclidean_distance):
 #Return a list of distances between vector a, and each row of X
 #USE distance_fn to calculate distances. eg: some_dist = distance_fn(a,b)
@@ -80,3 +90,33 @@ for i, image in enumerate(trainimages):
 print 'done!'
 # Go through the SIFTs of every image and create a histogram for the image
 # relative to the clusters you discovered in the previous phase.
+
+###Do the same as before, but now for the validation set      ###
+
+#Training ground truth labels
+train_labels = np.array([int(line.strip().split(" ")[1]) for
+                         line in open("trainset-overview.txt", "r")])
+
+#Validation images
+valimages = [line.split(' ')[0] for line in open('valset-overview.txt','r')]
+
+#Validation ground truth labels
+val_labels = np.array([int(line.rstrip().split(' ')[1]) for
+                       line in open('valset-overview.txt','r')])
+
+#Calculate the histogram representations for the validation images
+val_feat = np.zeros((len(valimages), size_of_histograms))
+
+for i, image in enumerate(valimages):
+    point1, image_sift = extract_sift(image)
+    for point in image_sift:
+        x = [euclidean_distance(point, clust) for clust in clusters]
+        clusterin = np.argmin(x)
+        val_feat[i][clusterin] += 1
+print 'done!'
+
+#to store the train and validation histograms. This allows us to play around
+#with feature selection, while saving the changes. Does overwrite itself, so
+#be careful!
+np.save('train_feat.npy',train_feat)
+np.save('val_feat.npy',val_feat)
